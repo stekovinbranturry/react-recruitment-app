443e7557105e18190c2fa3d45f4456493a340a2f
Zhang Kai <zhangkai930418@gmail.com>
2019-07-13 17:00:51 +0800

    Test mongodb

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div>App</div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			info: {}
+		};
+	}
+	componentDidMount() {
+		axios.get('/data').then(res => {
+			this.setState({ info: res.data });
+		});
+	}
+
+	render() {
+		return <div>hello {this.state.info.user}</div>;
+	}
 }
 
 export default App;
