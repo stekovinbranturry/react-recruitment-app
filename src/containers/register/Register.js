1569e8a2b684c9b9f4d2eadf46c5ea9067c570a0
ZHANG Kai <kai.zhang@soprasteria.com>
2019-07-16 17:48:11 +0800

    register component

---
+++ src/containers/register/Register.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import Logo from '../../components/Logo';
+
+class Register extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Logo />
+        Register
+      </Fragment>
+    );
+  }
+}
+
+export default Register;
