84ccdd30acfc6c5fe407fd997189fe5a9876d4e8
ZHANG Kai <kai.zhang@soprasteria.com>
2019-07-16 17:48:01 +0800

    login component

---
+++ src/containers/login/Login.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import Logo from '../../components/Logo';
+
+class Login extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Logo />
+        Login
+      </Fragment>
+    );
+  }
+}
+
+export default Login;
