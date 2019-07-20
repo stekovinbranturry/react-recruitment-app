cdec674e1461b179755e4bf2c0c3c006bd1e7e0d
Zhang Kai <zhangkai930418@gmail.com>
2019-07-20 23:44:00 +0800

    password encryption

--- server/user.js
+++ server/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Router = express.Router();
 const db = require('./db');
 const User = db.getModel('user');
+const utils = require('./utils');
 
 Router.get('/info', (req, res) => res.json({ code: 1 }));
 
@@ -16,8 +17,12 @@ Router.post('/register', (req, res) => {
 		if (doc) {
 			return res.json({ code: 1001, msg: '用户名已存在' });
 		}
-		User.create({ identity, phone, password }, (err, doc) =>
-			err ? res.json({ code: 1002, msg: '后台出错' }) : res.json({ code: 1000 })
+		User.create(
+			{ identity, phone, password: utils.md5Encryption(password) },
+			(err, doc) =>
+				err
+					? res.json({ code: 1002, msg: '后台出错' })
+					: res.json({ code: 1000 })
 		);
 	});
 });
