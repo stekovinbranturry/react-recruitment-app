ec1fdb209201f9ed42bde5749a6dc0a7ed7e49a0
Zhang Kai <zhangkai930418@gmail.com>
2019-07-21 16:40:00 +0800

    use cookie

--- server/user.js
+++ server/user.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const Router = express.Router();
 const db = require('./db');
-const User = db.getModel('user');
 const utils = require('./utils');
 
-Router.get('/info', (req, res) => res.json({ code: 1 }));
+const Router = express.Router();
+const User = db.getModel('user');
 
-Router.get('/list', (req, res) => {
-	User.find({}, (err, doc) => (err ? res.json(err) : res.json(doc)));
+const _filter = { password: 0, __v: 0 };
+
+/**
+ * User route redirect
+ */
+Router.get('/info', (req, res) => {
+	const { userid } = req.cookies;
+	if (!userid) {
+		return res.json({ code: 1 });
+	} else {
+		User.find({ _id: userid }, _filter, (err, doc) =>
+			doc
+				? res.json({ code: 0, doc })
+				: res.json({ code: 1, msg: 'userid不存在' })
+		);
+	}
 });
 
+/**
+ * User list
+ */
+Router.get('/list', (req, res) => {
+	User.find({}, _filter, (err, doc) => (err ? res.json(err) : res.json(doc)));
+});
+
+/**
+ * User register
+ */
 Router.post('/register', (req, res) => {
 	console.log(req.body);
 	const { identity, phone, password } = req.body;
@@ -17,23 +40,41 @@ Router.post('/register', (req, res) => {
 		if (doc) {
 			return res.json({ code: 1001, msg: '用户名已存在' });
 		}
-		User.create(
-			{ identity, phone, password: utils.md5Encryption(password) },
-			(err, doc) =>
-				err
-					? res.json({ code: 1002, msg: '后台出错' })
-					: res.json({ code: 1000 })
-		);
+		const userModel = new User({
+			identity,
+			phone,
+			password: utils.md5Encryption(password)
+		});
+
+		userModel.save((err, doc) => {
+			if (err) {
+				return res.json({ code: 1002, msg: '后台出错' });
+			} else {
+				const { _id, identity, phone } = doc;
+				res.cookie('userid', _id);
+				return res.json({ code: 1000, data: { _id, identity, phone } });
+			}
+		});
 	});
 });
 
+/**
+ * User login
+ */
 Router.post('/login', (req, res) => {
 	console.log(req.body);
 	const { phone, password } = req.body;
-	User.findOne({ phone, password: utils.md5Encryption(password) }, (err, doc) =>
-		doc
-			? res.json({ code: 1100, doc })
-			: res.json({ code: 1101, msg: '手机号或密码错误' })
+	User.findOne(
+		{ phone, password: utils.md5Encryption(password) },
+		_filter,
+		(err, doc) => {
+			if (doc) {
+				res.cookie('userid', doc._id);
+				return res.json({ code: 1100, doc });
+			} else {
+				res.json({ code: 1100, doc });
+			}
+		}
 	);
 });
 
--- src/components/AuthRouter.js
+++ src/components/AuthRouter.js
@@ -4,24 +4,28 @@ import { withRouter } from 'react-router-dom';
 
 @withRouter
 class AuthRouter extends Component {
-  componentDidMount() {
-    const pathList = ['/login', '/register'];
-    const currentPath = this.props.location.pathname;
-    if (!pathList.includes(currentPath)) {
-      axios.get('/user/info').then(res => {
-        if (res.status === 200) {
-          if (res.data.code === 0) {
-          } else {
-            this.props.history.push('/login');
-          }
-        }
-      });
-    }
-  }
+	componentDidMount() {
+		const pathList = ['/login', '/register'];
+		const currentPath = this.props.location.pathname;
+		if (!pathList.includes(currentPath)) {
+			axios.get('/user/info').then(res => {
+				if (res.status === 200 && res.data.code === 1) {
+					this.props.history.push('/login');
+				}
+			});
+		} else {
+			axios.get('/user/info').then(res => {
+				if (res.status === 200 && res.data.code === 0) {
+					const { identity } = res.data.doc[0];
+					this.props.history.push(`/${identity}-info`);
+				}
+			});
+		}
+	}
 
-  render() {
-    return null;
-  }
+	render() {
+		return null;
+	}
 }
 
 export default AuthRouter;
