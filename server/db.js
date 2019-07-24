9f78c17a4b8029c5879b30a6ebdbc9d68032dd34
Zhang Kai <zhangkai930418@gmail.com>
2019-07-25 01:44:31 +0800

    fix update seeker profile db bug

--- server/db.js
+++ server/db.js
@@ -1,33 +1,33 @@
 const mongoose = require('mongoose');
 
 const DB = 'mongodb://127.0.0.1:27017/react-recruitment-app';
-mongoose.connect(DB);
+mongoose.connect(DB, { useFindAndModify: false, useNewUrlParser: true });
 mongoose.connection.on('connected', () => console.log('Mongodb connected'));
 
 const models = {
-  user: {
-    phone: { type: String, required: true },
-    password: { type: String, required: true },
-    identity: { type: String, required: true },
-    avatar: String,
-    name: String,
-    age: Number,
-    education: String,
-    skills: String,
-    workExperience: String,
-    projectExperience: String,
-    position: String,
-    jobsHunting: Array
-  }
+	user: {
+		phone: { type: String, required: true },
+		password: { type: String, required: true },
+		identity: { type: String, required: true },
+		avatar: String,
+		name: String,
+		age: Number,
+		education: String,
+		skills: String,
+		workExperience: String,
+		projectExperience: String,
+		position: String,
+		jobsHunting: Array
+	}
 };
 
 for (const m in models) {
-  if (models.hasOwnProperty(m)) {
-    const el = models[m];
-    mongoose.model(m, new mongoose.Schema(el));
-  }
+	if (models.hasOwnProperty(m)) {
+		const el = models[m];
+		mongoose.model(m, new mongoose.Schema(el));
+	}
 }
 
 module.exports = {
-  getModel: name => mongoose.model(name)
+	getModel: name => mongoose.model(name)
 };
--- server/user.js
+++ server/user.js
@@ -11,137 +11,137 @@ const _filter = { password: 0, __v: 0 };
  * User route redirect
  */
 Router.get('/info', (req, res) => {
-  const { userid } = req.cookies;
-  if (!userid) {
-    return res.json({ code: 1 });
-  } else {
-    User.find({ _id: userid }, _filter, (err, doc) =>
-      doc
-        ? res.json({ code: 0, doc })
-        : res.json({ code: 1, msg: 'userid不存在' })
-    );
-  }
+	const { userid } = req.cookies;
+	if (!userid) {
+		return res.json({ code: 1 });
+	} else {
+		User.find({ _id: userid }, _filter, (err, doc) =>
+			doc
+				? res.json({ code: 0, doc })
+				: res.json({ code: 1, msg: 'userid不存在' })
+		);
+	}
 });
 
 /**
  * User list
  */
 Router.get('/list', (req, res) => {
-  User.find({}, _filter, (err, doc) => (err ? res.json(err) : res.json(doc)));
+	User.find({}, _filter, (err, doc) => (err ? res.json(err) : res.json(doc)));
 });
 
 /**
  * User register
  */
 Router.post('/register', (req, res) => {
-  console.log(req.body);
-  const { identity, phone, password } = req.body;
-  User.findOne({ phone }, (err, doc) => {
-    if (doc) {
-      return res.json({ code: 1001, msg: '用户名已存在' });
-    }
-    const userModel = new User({
-      identity,
-      phone,
-      password: utils.md5Encryption(password)
-    });
+	console.log(req.body);
+	const { identity, phone, password } = req.body;
+	User.findOne({ phone }, (err, doc) => {
+		if (doc) {
+			return res.json({ code: 1001, msg: '用户名已存在' });
+		}
+		const userModel = new User({
+			identity,
+			phone,
+			password: utils.md5Encryption(password)
+		});
 
-    userModel.save((err, doc) => {
-      if (err) {
-        return res.json({ code: 1002, msg: '后台出错' });
-      } else {
-        const { _id, identity, phone } = doc;
-        res.cookie('userid', _id);
-        return res.json({ code: 1000, data: { _id, identity, phone } });
-      }
-    });
-  });
+		userModel.save((err, doc) => {
+			if (err) {
+				return res.json({ code: 1002, msg: '后台出错' });
+			} else {
+				const { _id, identity, phone } = doc;
+				res.cookie('userid', _id);
+				return res.json({ code: 1000, data: { _id, identity, phone } });
+			}
+		});
+	});
 });
 
 /**
  * User login
  */
 Router.post('/login', (req, res) => {
-  console.log(req.body);
-  const { phone, password } = req.body;
-  User.findOne(
-    { phone, password: utils.md5Encryption(password) },
-    _filter,
-    (err, doc) => {
-      if (doc) {
-        res.cookie('userid', doc._id);
-        return res.json({ code: 1100, doc });
-      } else {
-        res.json({ code: 1101, msg: '用户名或密码错误' });
-      }
-    }
-  );
+	console.log(req.body);
+	const { phone, password } = req.body;
+	User.findOne(
+		{ phone, password: utils.md5Encryption(password) },
+		_filter,
+		(err, doc) => {
+			if (doc) {
+				res.cookie('userid', doc._id);
+				return res.json({ code: 1100, doc });
+			} else {
+				res.json({ code: 1101, msg: '用户名或密码错误' });
+			}
+		}
+	);
 });
 
 /**
  * upload avatar
  */
 Router.post('/uploadAvatar', (req, res) => {
-  console.log(req.body);
-  const { userid } = req.cookies;
-  const { avatar } = req.body;
-  if (avatar) {
-    User.findByIdAndUpdate({ _id: userid }, { avatar }, (err, doc) =>
-      doc
-        ? res.json({ code: 1200, msg: 'Avatar上传成功' })
-        : res.json({ code: 1201, msg: 'Avatar上传失败' })
-    );
-  }
+	console.log(req.body);
+	const { userid } = req.cookies;
+	const { avatar } = req.body;
+	if (avatar) {
+		User.findByIdAndUpdate({ _id: userid }, { avatar }, (err, doc) =>
+			doc
+				? res.json({ code: 1200, msg: 'Avatar上传成功' })
+				: res.json({ code: 1201, msg: 'Avatar上传失败' })
+		);
+	}
 });
 /**
  * Update profile
  */
 Router.post('/update', (req, res) => {
-  console.log(req.body);
-  const { userid } = req.cookies;
-  const {
-    position,
-    name,
-    age,
-    education,
-    skills,
-    workExperience,
-    projectExperience,
-    jobsHunting
-  } = req.body;
-  if (position) {
-    User.findByIdAndUpdate(
-      { _id: userid },
-      {
-        position,
-        name,
-        age,
-        education,
-        skills,
-        workExperience,
-        projectExperience
-      },
-      (err, doc) =>
-        doc
-          ? res.json({ code: 1202, msg: '简历更新成功' })
-          : res.json({ code: 1203, msg: '简历更新失败' })
-    );
-  }
-  if (jobsHunting) {
-    User.updateOne({ _id: userid }, { $push: { jobsHunting } }, (err, doc) =>
-      doc ? res.json(doc) : res.json(err)
-    );
-  }
+	console.log(req.body);
+	const { userid } = req.cookies;
+	const {
+		position,
+		name,
+		age,
+		education,
+		skills,
+		workExperience,
+		projectExperience,
+		jobsHunting
+	} = req.body;
+	if (position) {
+		User.findOneAndUpdate(
+			{ _id: userid },
+			{
+				position,
+				name,
+				age,
+				education,
+				skills,
+				workExperience,
+				projectExperience
+			},
+			(err, doc) =>
+				doc
+					? res.json({ code: 1202, msg: '简历更新成功' })
+					: res.json({ code: 1203, msg: '简历更新失败' })
+		);
+	}
+	if (jobsHunting.length !== 0) {
+		User.updateOne({ _id: userid }, { $push: { jobsHunting } }, (err, doc) =>
+			doc ? res.json(doc) : res.json(err)
+		);
+	}
 });
 
 /**
  * User query
  */
 Router.post('/query', (req, res) => {
-  const { userid } = req.cookies;
-  User.findOne({ _id: userid }, _filter, (err, doc) =>
-    doc ? res.json({ code: 1300, doc }) : res.json({ code: 1301, doc })
-  );
+	const { userid } = req.cookies;
+	User.findOne({ _id: userid }, _filter, (err, doc) =>
+		doc ? res.json({ code: 1300, doc }) : res.json({ code: 1301, doc })
+	);
 });
 
 module.exports = Router;
