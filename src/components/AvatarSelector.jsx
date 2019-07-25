5955abdc019bbcf01a9bc9490f59609e6f5060a3
Zhang Kai <zhangkai930418@gmail.com>
2019-07-26 00:21:11 +0800

    add proptypes

--- src/components/AvatarSelector.jsx
+++ src/components/AvatarSelector.jsx
@@ -1,56 +1,62 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import { Grid, Flex, Toast } from 'antd-mobile';
 
 const AvatarSelector = props => {
-  const avatars = [
-    'boy',
-    'bull',
-    'chick',
-    'crab',
-    'girl',
-    'hedgehog',
-    'hippopotamus',
-    'koala',
-    'lemur',
-    'man',
-    'pig',
-    'tiger',
-    'whale',
-    'woman',
-    'zebra'
-  ];
+	const avatars = [
+		'boy',
+		'bull',
+		'chick',
+		'crab',
+		'girl',
+		'hedgehog',
+		'hippopotamus',
+		'koala',
+		'lemur',
+		'man',
+		'pig',
+		'tiger',
+		'whale',
+		'woman',
+		'zebra'
+	];
 
-  const data = avatars.map(item => ({
-    icon: require(`../image/avatars/${item}.png`),
-    text: item
-  }));
+	const data = avatars.map(item => ({
+		icon: require(`../image/avatars/${item}.png`),
+		text: item
+	}));
 
-  const { avatar, setAvatar } = props;
+	const { avatar, setAvatar } = props;
 
-  const uploadAvatar = avatar => {
-    axios
-      .post('/user/uploadAvatar', { avatar })
-      .then(res =>
-        res.data.code === 1200 ? setAvatar(avatar) : Toast.info(res.data.msg)
-      )
-      .catch(err => console.log(err));
-  };
+	const uploadAvatar = avatar => {
+		axios
+			.post('/user/uploadAvatar', { avatar })
+			.then(res =>
+				res.data.code === 1200 ? setAvatar(avatar) : Toast.info(res.data.msg)
+			)
+			.catch(err => console.log(err));
+	};
 
-  return avatar ? (
-    <Flex justify='center'>
-      <img
-        className='avatar'
-        src={require(`../image/avatars/${avatar}.png`)}
-        alt={avatar}
-      />
-    </Flex>
-  ) : (
-    <div>
-      <div className='input-title'>请选择头像：</div>
-      <Grid data={data} columnNum={5} onClick={_el => uploadAvatar(_el.text)} />
-    </div>
-  );
+	return avatar ? (
+		<Flex justify="center">
+			<img
+				className="avatar"
+				src={require(`../image/avatars/${avatar}.png`)}
+				alt={avatar}
+			/>
+		</Flex>
+	) : (
+		<div>
+			<div className="input-title">请选择头像：</div>
+			<Grid data={data} columnNum={5} onClick={_el => uploadAvatar(_el.text)} />
+		</div>
+	);
+};
+
+AvatarSelector.propTypes = {
+	avatar: PropTypes.string,
+	setAvatar: PropTypes.func
 };
 
 export default AvatarSelector;
