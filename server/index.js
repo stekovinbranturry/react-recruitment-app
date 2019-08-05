1dc0611f6b2cbda1ce694b69e143ec259edb2fde
ZHANG Kai <kai.zhang@soprasteria.com>
2019-08-05 18:30:53 +0800

    chat input

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const http = require('http');
+const socketIo = require('socket.io');
 const userRouter = require('./user');
 
 const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 const port = 3001;
+
+io.on('connection', () => console.log('user login'));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use('/user', userRouter);
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
--- src/containers/message/Chat.jsx
+++ src/containers/message/Chat.jsx
@@ -1,14 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import io from 'socket.io-client';
+import { List, InputItem, Icon, WingBlank } from 'antd-mobile';
+import { withRouter } from 'react-router-dom';
 import MainNavBar from '../../components/MainNavBar.jsx';
 
 const Chat = props => {
 	const { user } = props.match.params;
+	const socket = io('ws://localhost:3001');
+	const [input, setInput] = useState('');
 	return (
 		<Fragment>
 			<MainNavBar title={`正在与${user}聊天`} />
-			<div>chat with: {user}</div>
+			<Icon
+				type='left'
+				size='lg'
+				onClick={() => {
+					props.history.push('/message');
+				}}
+			/>
+			<WingBlank>
+				<List className='chat-input'>
+					<InputItem value={input} extra='发送' onChange={v => setInput(v)} />
+				</List>
+			</WingBlank>
 		</Fragment>
 	);
 };
 
-export default Chat;
+export default withRouter(Chat);
